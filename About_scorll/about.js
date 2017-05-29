0e0686d869ffe9ad6932710f7389a87b53ada132
sh-byeon <bun920528@gmail.com>
2017-05-29 18:56:29 +0900

    complete scroll

--- About_scorll/about.js
+++ About_scorll/about.js
@@ -8,207 +8,152 @@ var gray = document.querySelector(".gray");
 
 
 // function <-> switch
-// $(red).on("mousewheel", function (event) {
-//   if( event.originalEvent.wheelDelta < 0 ) {
-//     red.classList.add( 'old-hat' );
-//     console.log("Time",(new Date()).getTime())
-//   }
-//
-//   });
-// $(blue).on("mousewheel", function (event) {
-//   if(event.originalEvent.wheelDelta < 0) {
-//     blue.classList.add( 'old-hat' );
-//   } else {
-//     red.classList.remove( 'old-hat' );
-//   }
-//   });
-// $(yellow).on("mousewheel", function (event) {
-//   if(event.originalEvent.wheelDelta < 0) {
-//     yellow.classList.add( 'old-hat' );
-//   } else {
-//     blue.classList.remove( 'old-hat' );
-//   }
-//   });
-//
-// $(gray).on("mousewheel", function (event) {
-//   if(event.originalEvent.wheelDelta > 0) {
-//     yellow.classList.remove( 'old-hat' );
-//     }
-//   });
-
-//**********************경계선***********************
-
-
-var color = [red, blue, yellow, gray];
-var wrap = $("body");
-console.log(wrap)
-var panelsize = 100;
-var step = 10;
-var interval = 1000;
-var direction = 1;
-var currentTop = parseInt(wrap.css("top"));
-var bAnimation = false;
-function animation() {
-  setTimeout(function() {
-
-
-    if (direction < 0) {
-      if (currentTop <= minValue) {
-        setTimeout(function() {
-          bAnimation = false;
-        }, interval);
-        return;
-      }
-    } else {
-      if (currentTop >= minValue) {
-        setTimeout(function() {
-          bAnimation = false;
-        }, interval);
-        return;
-      }
-    }
-
-    wrap.css({
-      "top": currentTop - step
-    });
-    animation();
-  }, 16);
-}
-
-for ( i = 0; i < color.length ; i++ ){
-
-
-
-switch ( color[i] ) {
-
-
-
-case red: $(red).on("mousewheel DOMMouseScroll", function (event) {
+$(red).on("mousewheel", function (event) {
+  console.log('red');
   if( event.originalEvent.wheelDelta < 0 ) {
-   event.preventDefault();
-  minValue = currentTop - panelsize;
-red.classList.add( 'old-hat' ); }
-   animation()
-console.log('hellow')
+
+    red.classList.add( 'old-hat' );
+
+    setTimeout(function() {
+        blue.classList.add('completely');
+    }, 1200);
+
+  }
 
   });
-
-  break;
-
-
-case blue: $(blue).on("mousewheel DOMMouseScroll", function (event) {
-
-
-  if(event.originalEvent.wheelDelta < 0 ) {
-     event.preventDefault();
-      minValue = currentTop - panelsize;
+$(blue).on("mousewheel", function (event) {
+  if(event.originalEvent.wheelDelta < 0 && $(blue).hasClass('completely') === true ) {
+    console.log('blue');
     blue.classList.add( 'old-hat' );
+    blue.classList.remove( 'completely')
+    setTimeout(function() {
+        yellow.classList.add('completely');
+    }, 1200);
 
-     animation()
-
-
-  } else {
+  } else if ( event.originalEvent.wheelDelta > 0 && $(blue).hasClass('completely') === true ){
     red.classList.remove( 'old-hat' );
-     animation()
+    blue.classList.remove( 'completely')
 
   }
   });
 
-  break;
-
-case yellow: $(yellow).on("mousewheel DOMMouseScroll", function (event) {
-
-  if(event.originalEvent.wheelDelta < 0 ) {
-     event.preventDefault();
-     minValue = currentTop - panelsize;
-
+$(yellow).on("mousewheel", function (event) {
+  if(event.originalEvent.wheelDelta < 0 && $(yellow).hasClass('completely') === true) {
+    console.log("$(yellow).hasClass('completely') === true :", $(yellow).hasClass('completely') === true)
     yellow.classList.add( 'old-hat' );
-     animation()
+    yellow.classList.remove( 'completely');
+    setTimeout(function() {
+        gray.classList.add('completely');
+    }, 1200);
 
-
-  } else {
-    blue.classList.remove( 'old-hat' );
-    animation()
-   }
-
+  } else if (event.originalEvent.wheelDelta > 0 && $(yellow).hasClass('completely') === true){
+        blue.classList.remove( 'old-hat' );
+        yellow.classList.remove( 'completely')
+    setTimeout(function() {
+      blue.classList.add('completely');
+    }, 1200);
+  }
   });
-  break;
 
-case gray: $(gray).on("mousewheel DOMMouseScroll", function (event) {
-
-  if(event.originalEvent.wheelDelta > 0) {
-     event.preventDefault();
-     minValue = currentTop - panelsize;
+$(gray).on("mousewheel", function (event) {
+  if(event.originalEvent.wheelDelta > 0 && $(gray).hasClass('completely') === true) {
     yellow.classList.remove( 'old-hat' );
-    animation();
+    gray.classList.remove('completely');
+
+    setTimeout(function() {
+      yellow.classList.add('completely');
+    }, 1200);
+
     }
   });
-  break;
-  animation()
- }
-}
+
+//**********************경계선***********************
 
 
+// var color = [red, blue, yellow, gray];
 //
-//################################# 경계선 #############################
 //
-// (function() {
-//   var currentPanel = 1;
-//   var wrap = $('body');
-//   var panelsize = 600;
-//   var step = 10;
-//   var interval = 1000;
-//   var direction = 1;
+// for ( i = 0; i < color.length ; i++ ){
 //
-//   var bAnimation = false;
+// switch ( color[i] ) {
 //
-//   function animation() {
-//     setTimeout(function() {
-//       var currentTop = parseInt(wrap.css("old-hat"));
 //
-//       if (direction < 0) {
-//         if (currentTop <= minValue) {
-//           setTimeout(function() {
-//             bAnimation = false;
-//           }, interval);
-//           return;
-//         }
-//       } else {
-//         if (currentTop >= minValue) {
-//           setTimeout(function() {
-//             bAnimation = false;
-//           }, interval);
-//           return;
-//         }
-//       }
 //
-//       wrap.css({
-//         "old-hat": currentTop - step
-//       });
-//       animation();
-//     }, 16);
+// case red: $(red).on("mousewheel", function (event) {
+//   if( event.originalEvent.wheelDelta < 0 ) {
+//
+//
+// red.classList.add( 'old-hat' );
+//
+// setTimeout(function() {
+//     blue.classList.add('completely');
+// }, 700);
+//
+//
+// }
+//
+//   });
+//
+//
+//
+// break;
+//
+//
+// case blue: $(blue).on("mousewheel", function (event) {
+//
+//
+//   if(event.originalEvent.wheelDelta < 0 && blue.hasClass("completely") === true)
+//      event.preventDefault();
+//
+//     blue.classList.add( 'old-hat' );
+//     blue.classList.remove('completely');
+//     $(document).ready(function () {
+//
+//    setTimeout(function() {
+//        yellow.classList.add("completely");
+//    }, 700);
+// })
+//   } else {
+//     red.classList.remove( 'old-hat' );
 //   }
 //
-//   $(window).bind('mousewheel DOMMouseScroll', function(event) {
-//     event.preventDefault();
-//     if (bAnimation) return;
-//
-//     var currentTop = parseInt(wrap.css("old-hat"));
-//
-//     if (event.originalEvent.wheelDelta < 0) {
-//       //down scroll
-//       minValue = currentTop - panelsize;
-//       step = 10;
-//       direction = -1;
-//     } else {
-//       //up scroll
-//       minValue = currentTop + panelsize;
-//       step = -10;
-//       direction = 1;
-//     }
-//
-//     console.log(minValue, bAnimation);
-//     bAnimation = true;
-//     animation();
 //   });
-// })();
+//
+//   break;
+//
+// case yellow: $(yellow).on("mousewheel", function (event) {
+//
+//   if(event.originalEvent.wheelDelta < 0 && yellow.hasClass("completely") === true) {
+//      event.preventDefault();
+//
+//
+//     yellow.classList.add( 'old-hat' );
+//     yellow.classList.remove('completely');
+//     setTimeout(function() {
+//         gary.classList.add("completely");
+//     }, 700);
+//  });
+//
+//
+//
+//   } else {
+//     blue.classList.remove( 'old-hat' );
+//
+//    }
+//
+//   });
+//   break;
+//
+// case gray: $(gray).on("mousewheel", function (event) {
+//
+//   if(event.originalEvent.wheelDelta > 0 && yellow.hasClass("completely") === true) {
+//      event.preventDefault();
+//
+//     yellow.classList.remove( 'old-hat' );
+//
+//     }
+//   });
+//   break;
+//
+//  }
+// }
