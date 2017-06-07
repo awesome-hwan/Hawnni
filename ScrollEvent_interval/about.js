e3058ebb115d5cbbf6ab91abd2826f4cd1311831
sh-byeon <bun920528@gmail.com>
2017-06-07 13:51:01 +0900

    Click&&Scrolling event add

--- ScrollEvent_interval/about.js
+++ ScrollEvent_interval/about.js
@@ -1,67 +1,164 @@
 
 
-var red = document.querySelector(".red");
-var blue = document.querySelector(".blue");
-var yellow = document.querySelector(".yellow");
-var gray = document.querySelector(".gray");
+var first = document.querySelector(".first");
+var second = document.querySelector(".second");
+var third = document.querySelector(".third");
+var four = document.querySelector(".four");
+
+// dot의 상태변경
+var ulistChild = document.querySelector('.status');
+var dot = ulistChild.children
 
 
+dot[0].onclick = function () {
+  dot[0].classList.add('active');
+  first.classList.add('completely');
+
+  dot[1].classList.remove('active');
+  dot[2].classList.remove('active');
+  dot[3].classList.remove('active');
+
+  first.classList.remove('old-hat');
+  second.classList.remove('old-hat');
+
+  second.classList.remove('completely');
+  third.classList.remove('completely');
+  four.classList.remove('completely');
+
+}
+dot[1].onclick = function () {
+  dot[1].classList.add('active');
+  second.classList.add('completely');
+
+  dot[0].classList.remove('active');
+  dot[2].classList.remove('active');
+  dot[3].classList.remove('active');
+
+  //old-hat
+  first.classList.add('old-hat');
+
+  second.classList.remove('old-hat')
+  third.classList.remove('old-hat')
+
+  first.classList.remove('completely');
+  third.classList.remove('completely');
+  four.classList.remove('completely');
+}
+dot[2].onclick = function () {
+  dot[2].classList.add('active');
+  third.classList.add('completely');
+
+  dot[0].classList.remove('active');
+  dot[1].classList.remove('active');
+  dot[3].classList.remove('active');
+
+  first.classList.add('old-hat');
+  second.classList.add('old-hat');
+
+  third.classList.remove('old-hat')
+
+  first.classList.remove('completely');
+  second.classList.remove('completely');
+  four.classList.remove('completely');
+}
+dot[3].onclick = function () {
+  dot[3].classList.add('active');
+  four.classList.add('completely');
+
+  dot[0].classList.remove('active');
+  dot[1].classList.remove('active');
+  dot[2].classList.remove('active');
+
+
+
+  first.classList.add('old-hat');
+  second.classList.add('old-hat');
+  third.classList.add('old-hat');
+
+  first.classList.remove('completely');
+  second.classList.remove('completely');
+  third.classList.remove('completely');
+}
 
 // function <-> switch
-$(red).on("mousewheel", function (event) {
-  
-  if( event.originalEvent.wheelDelta < 0 ) {
+$(first).on("mousewheel", function (event) {
 
-    red.classList.add( 'old-hat' );
+  if( event.originalEvent.wheelDelta < 0 && $(first).hasClass('completely') === true ) {
+
+    first.classList.add( 'old-hat' );
+    first.classList.remove('completely');
+
+
+    dot[0].classList.remove('active');
+    dot[1].classList.add('active');
 
     setTimeout(function() {
-        blue.classList.add('completely');
+        second.classList.add('completely');
     }, 1500);
 
   }
 
   });
-$(blue).on("mousewheel", function (event) {
-  if(event.originalEvent.wheelDelta < 0 && $(blue).hasClass('completely') === true ) {
-    
-    blue.classList.add( 'old-hat' );
-    blue.classList.remove( 'completely')
+$(second).on("mousewheel", function (event) {
+  if(event.originalEvent.wheelDelta < 0 && $(second).hasClass('completely') === true ) {
+
+    second.classList.add( 'old-hat' );
+    second.classList.remove( 'completely');
+
+    dot[1].classList.remove('active');
+    dot[2].classList.add('active');
+
     setTimeout(function() {
-        yellow.classList.add('completely');
+        third.classList.add('completely');
     }, 1500);
 
-  } else if ( event.originalEvent.wheelDelta > 0 && $(blue).hasClass('completely') === true ){
-    red.classList.remove( 'old-hat' );
-    blue.classList.remove( 'completely')
+  } else if ( event.originalEvent.wheelDelta > 0 && $(second).hasClass('completely') === true ){
+    first.classList.remove( 'old-hat' );
+    second.classList.remove( 'completely')
+
+    dot[1].classList.remove('active');
+    dot[0].classList.add('active');
+
+    setTimeout(function() {
+        first.classList.add('completely');
+    }, 1500);
 
   }
   });
 
-$(yellow).on("mousewheel", function (event) {
-  if(event.originalEvent.wheelDelta < 0 && $(yellow).hasClass('completely') === true) {
-    
-    yellow.classList.add( 'old-hat' );
-    yellow.classList.remove( 'completely');
+$(third).on("mousewheel", function (event) {
+  if(event.originalEvent.wheelDelta < 0 && $(third).hasClass('completely') === true) {
+
+    third.classList.add( 'old-hat' );
+    third.classList.remove( 'completely');
+
+    dot[2].classList.remove('active');
+    dot[3].classList.add('active');
     setTimeout(function() {
-        gray.classList.add('completely');
+        four.classList.add('completely');
     }, 1500);
 
-  } else if (event.originalEvent.wheelDelta > 0 && $(yellow).hasClass('completely') === true){
-        blue.classList.remove( 'old-hat' );
-        yellow.classList.remove( 'completely')
+  } else if (event.originalEvent.wheelDelta > 0 && $(third).hasClass('completely') === true){
+        second.classList.remove( 'old-hat' );
+        third.classList.remove( 'completely');
+
+        dot[2].classList.remove('active');
+        dot[1].classList.add('active');
     setTimeout(function() {
-      blue.classList.add('completely');
+      second.classList.add('completely');
     }, 1500);
   }
   });
 
-$(gray).on("mousewheel", function (event) {
-  if(event.originalEvent.wheelDelta > 0 && $(gray).hasClass('completely') === true) {
-    yellow.classList.remove( 'old-hat' );
-    gray.classList.remove('completely');
+$(four).on("mousewheel", function (event) {
+  if(event.originalEvent.wheelDelta > 0 && $(four).hasClass('completely') === true) {
+    third.classList.remove( 'old-hat' );
+    four.classList.remove('completely');
 
+    dot[3].classList.remove('active');
+    dot[2].classList.add('active');
     setTimeout(function() {
-      yellow.classList.add('completely');
+      third.classList.add('completely');
     }, 1500);
 
     }
